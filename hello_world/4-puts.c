e790361e55581e0df382c85ff5316db51f916c33
masterry <terry.al.khoury@gmail.com>
2024-02-19 23:50:08 +0200

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
-
-int main()
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
