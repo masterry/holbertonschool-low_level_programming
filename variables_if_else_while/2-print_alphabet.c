74ffcc239a2e138506028aedbfb2ca8a67ed3d7a
masterry <terry.al.khoury@gmail.com>
2024-02-21 23:05:46 +0200

    2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-  char i;
-  for (i = 'a' ; i <= 'z' ; i++)
-    putchar(i);
-  putchar('\n');
-  return (0);
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
 }
