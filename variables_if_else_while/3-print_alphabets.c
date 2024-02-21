de40f2bd82077500a0444bae9b8d4485f325444e
masterry <terry.al.khoury@gmail.com>
2024-02-21 23:10:13 +0200

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,15 @@ int main(void)
 char ch = 'a';
 char CH = 'A';
 while (ch <= 'z')
-	{
-		putchar(ch);
-		++ch;
-	}
-
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		++CH;
-	}
-	putchar('\n');
-	return (0);
+{
+putchar(ch);
+++ch;
+}
+while (CH <= 'Z')
+{
+putchar(CH);
+++CH;
+}
+putchar('\n');
+return (0);
 }
