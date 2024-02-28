f68d284df2a8e5c913c2551a6fbdbfb1bd12ff80
masterry <terry.al.khoury@gmail.com>
2024-02-28 12:19:44 +0200

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,11 +17,11 @@ void puts_half(char *str)
 
 	if (l % 2 == 0)
 	{
-	  for (n = 0 ; n < (l / 2) ; ++n)
+		for (n = l / 2; str[n] != '\0'; ++n)
 			_putchar(str[n]);
 	} else
 	{
-	  for (n = 0; n <= (l / 2); ++n)
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
 			_putchar(str[n]);
 	}
 	_putchar('\n');
