8841410824f352c734a72004211d89bc6bf769cf
masterry <terry.al.khoury@gmail.com>
2024-04-05 10:53:01 +0300

    0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,24 @@
 #include "lists.h"
+#include <stdio.h>
+
 /**
- * print_listint - a function that prints all the elements of a listint_t list.
- * @h: struct
- * Return: number of nodes
+ * print_dlistint - prints all elements of a dlistint_t list.
+ * @h: head of a doubly linked list
+ * Return: the number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	const dlistint_t *ptr = h;
+	size_t n = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (ptr && ptr->prev)
+		ptr = ptr->prev;
+	while (ptr)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+		printf("%d\n", ptr->n);
+		n++;
+		ptr = ptr->next;
 	}
-
-	return (count);
+	return (n);
 }
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,34 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
-*/
 typedef struct dlistint_s
 {
 	int n;
+	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
 
-int _putchar(char c);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_nodeint(dlistint_t **head, const int n);
-dlistint_t *add_nodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
-void free_dlistint2(dlistint_t **head);
-int pop_dlistint(dlistint_t **head);
-dlistint_t *get_nodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_nodeint_at_index(dlistint_t **head, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+#endif
